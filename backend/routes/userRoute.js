41306c33008c014d257e60b2584cd45aac5a8443
DaanSolo <DaanSolo@users.noreply.github.com>
2025-01-07 17:37:24 +0000

    add route get user recipes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import Joi from "joi";
 import { createValidator } from "express-joi-validation";
-import { list, get, remove, update } from '../controllers/userController.js';
+import { list, get, remove, update, listRecipes } from '../controllers/userController.js';
+import authenticated from "../middlewares/authenticated.js";
 
 const router = express.Router();
 const validator = createValidator({passError: true, statusCode: 400});
@@ -20,11 +21,16 @@ const userUpdateSchemaBody = Joi.object({
     name: Joi.string().optional(),
     email: Joi.string().optional(),
 });
-router.put('/:id', validator.params(userUpdateSchema), validator.body(userUpdateSchemaBody), update);
+router.put('/:id', validator.params(userUpdateSchema), validator.body(userUpdateSchemaBody), authenticated, update);
 
 const userDeleteSchema = Joi.object({
     id: Joi.string().required(),
 });
-router.delete('/:id', validator.params(userDeleteSchema), remove);
+router.delete('/:id', validator.params(userDeleteSchema), update, remove);
+
+const userRecipeSchema = Joi.object({
+    id: Joi.string().required(),
+});
+router.get('/:id/recipe', validator.params(userRecipeSchema), listRecipes);
 
 export default router;
